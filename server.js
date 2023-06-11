14e78c6e6cde05e3d8718dbe1ce93b6fbf399370
Amanda Gomes <amanda@MacBook-Pro.local>
2023-06-11 16:28:19 -0300

    Estruturação do projeto com Node Express

---
+++ server.js
@@ -0,0 +1,37 @@
+var express = require('express');
+var bodyParser = require('body-parser')
+var User = require('./src/classes/user.js'); 
+
+var app = express()
+ 
+// create application/json parser
+var jsonParser = bodyParser.json()
+ 
+// create application/x-www-form-urlencoded parser
+var urlencodedParser = bodyParser.urlencoded({ extended: false })
+
+// set the view engine to ejs
+app.set('view engine', 'ejs');
+
+// load up an ejs view file
+app.get('/', function(req, res) {
+    res.render('pages/index');
+});
+app.get('/about', function(req, res) {
+    res.render('pages/about');
+});
+app.get('/cadastro', function(req, res) {
+    res.render('pages/register');
+});
+app.post('/cadastro', urlencodedParser, function(req, res,){
+    let {nome, email, password} = req.body;
+    let usuario = new User(nome, email, password);
+    usuario.add(req.body);
+    res.render('pages/index');
+});
+
+// load public folder
+app.use(express.static(__dirname + '/public'));
+
+app.listen(8080);
+console.log('8080 is the magic port');
---
+++ src/classes/user.js
@@ -0,0 +1,40 @@
+// Classe Usuário
+module.exports = class User {
+  constructor(nome, email, senha) {
+    this.nome = nome;
+    this.email = email;
+    this.senha = senha;
+  }
+
+  async add() {
+    const objUser = { 
+      ...this
+    };
+  
+    const response = await fetch("http://localhost:3000/users", {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json",
+      },
+      body: JSON.stringify(objUser),
+    });
+
+    const result = await response.json();
+    console.log("Usuário adicionado: " + result);
+  }
+
+  async edit(id) {
+    const objUser = { ...this };
+
+    const res = await fetch(`http://localhost:3000/users/${id}`, {
+      method: "PUT",
+      headers: {
+        "Content-type": "application/json",
+      },
+      body: JSON.stringify(objUser),
+    });
+
+    const data = await res.json();
+    console.log("Usuário editado: " + data);
+  }
+}
