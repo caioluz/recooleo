0e979a788b4ecb925e47748412481eca013db34e
Amanda Gomes <amanda@MacBook-Pro.local>
2023-06-13 00:19:50 -0300

    Criação das telas iniciais

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,8 +1,13 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: ["./views/**/*.ejs"],
+  content: [
+    "./views/**/*.ejs",
+    "./node_modules/flowbite/**/*.js"
+  ],
   theme: {
     extend: {},
   },
-  plugins: [],
+  plugins: [
+    require('flowbite/plugin')
+  ],
 };
